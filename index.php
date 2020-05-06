fa03858f5fb97d090938bd6fbd78ed70def804d9
Guilherme Costa <guilherme3394@gmail.com>
2020-05-05 23:20:03 -0300

    Estiliza e adiciona responsividade a alguns elementos

--- index.php
+++ index.php
@@ -3,6 +3,9 @@
 
 <head>
 	<title>Projeto 01</title>
+	<link href="estilo/all.css" rel="stylesheet"> <!--load all styles -->
+	<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;700&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="estilo/style.css">
 	<meta charsert="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta name="keywords" content="palvras, chaves do, meu, site">
@@ -13,9 +16,9 @@
 	<header>
 		<div class="center">
 
-			<div class="logo">Logomarcar</div>
+			<div class="logo left"><i class="fab fa-gg-circle"></i></div>
 			<!--logo-->
-			<nav class="desktop">
+			<nav class="desktop rigth">
 				<ul>
 					<li><a href="">Home</a></li>
 					<li><a href="">Sobre</a></li>
@@ -23,7 +26,10 @@
 					<li><a href="">Contato</a></li>
 				</ul>
 			</nav>
-			<nav class="mobile">
+			<nav class="mobile rigth">
+				<div class="botao-menu-mobile">
+					<i class="fas fa-bars"></i>
+				</div>
 				<ul>
 					<li><a href="">Home</a></li>
 					<li><a href="">Sobre</a></li>
@@ -31,10 +37,12 @@
 					<li><a href="">Contato</a></li>
 				</ul>
 			</nav>
+			<div class="clear"></div>
 		</div>
 	</header>
 
 	<section class="banner-principal">
+		<div class="overlay"></div>
 		<div class="center">
 			<!--banner principal-->
 			<form>
@@ -47,7 +55,7 @@
 	<section class="descricao-autor">
 		<div class="center">
 			<!-- descricao-autor -->
-			<div class="w50">
+			<div class="w50 left">
 				<h2>Guilherme Costa</h2>
 				<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
 					the
@@ -56,7 +64,7 @@
 					and scrambled it to make a type specimen book. It has survived not only five centuries, but also
 					the
 					leap into electronic typesetting, remaining essentially unchanged. It was popularised in the
-					1960s
+					196w33 0s
 					with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with
 					desktop
 					publishing software like Aldus PageMaker including versions of Lorem Ipsum.
@@ -74,18 +82,19 @@
 					publishing software like Aldus PageMaker including versions of Lorem Ipsum.
 				</p>
 			</div>
-			<div class="w50">
+			<div class="w50 left">
 				<!-- pegar imagem depois -->
-				<img src="images/author.jpg" alt="">
+				<img class="rigth" src="images/author.jpg" alt="">
 			</div>
+			<div class="clear"></div>
 		</div>
 	</section>
 	<section>
 		<div class="center">
 			<h2 class="title">Especialidades</h2>
-			<div class="box-expecialidades">
-				<h3>Icone1</h3>
-				<h3>CSS3</h3>
+			<div class="w33 left box-expecialidades">
+				<h3><i class="fab fa-html5"></i></h3>
+				<h3>HTML</h3>
 				<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
 					the
 					industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of
@@ -99,8 +108,9 @@
 					publishing software like Aldus PageMaker including versions of Lorem Ipsum.
 				</p>
 			</div>
-			<div class="box-expecialidades">
-				<h3>Icone2</h3>
+			<div class="w33 left box-expecialidades">
+				<h3><i class="fab fa-js"></i>
+				</h3>
 				<h3>JAVASCRIPT</h3>
 				<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
 					the
@@ -115,8 +125,8 @@
 					publishing software like Aldus PageMaker including versions of Lorem Ipsum.
 				</p>
 			</div>
-			<div class="box-expecialidades">
-				<h3>Icone3</h3>
+			<div class="w33 left box-expecialidades">
+				<h3><i class="fab fa-php"></i></h3>
 				<h3>PHP</h3>
 				<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
 					the
@@ -135,7 +145,7 @@
 	</section>
 	<section class="extras">
 		<div class="center">
-			<div class="w50">
+			<div class="w50 left ">
 				<h2 class="title">Depoimentos</h2>
 				<div class="depoimento-single">
 					<p class="depoimento-descricao">
@@ -144,7 +154,7 @@
 						industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of
 						type
 						and scrambled it to make a type specimen book. It has survived not only five centuries, but also
-						the
+						tw33 he
 						leap into electronic typesetting, remaining essentially unchanged. It was popularised in the
 						1960s
 						with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with
@@ -186,7 +196,7 @@
 					<p class="nome-autor">Logrein Jun</p>
 				</div>
 			</div>
-			<div class="w50">
+			<div class="w50 left">
 				<h2 class="title">Serviços</h2>
 				<div class="servicos">
 					<ul>
@@ -195,7 +205,7 @@
 							the
 							industry's standard dummy text ever since the 1500s, when an unknown printer took a galley
 							of
-							type
+							w33 type
 						</li>
 						<li>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
 							been
@@ -228,6 +238,7 @@
 					</ul>
 				</div>
 			</div>
+			<div class="clear"></div>
 		</div>
 	</section>
 	<footer>
